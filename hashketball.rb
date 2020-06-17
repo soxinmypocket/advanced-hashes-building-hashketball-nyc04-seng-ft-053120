fa239a635202fb904c45b0ee5ebdd8f72bf12ebd
Shawnae J <shawnaejohnson6@gmail.com>
2020-06-17 18:53:20 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,8 +1,25 @@
 # Write your code here!
 def game_hash(hash)
 #top level has two keys :home and :away
-  hash = {:home => {:team_name => "", :colors => [] , :players => [{}]},
-          :away => {}}
+  hash = {
+    :home => {
+       :team_name => "",
+       :colors => [] ,
+       :players => [{:player_name => },
+                     {:number => },
+                     {:shoe => },
+                     {:points => },
+                     {:rebounds => },
+                     {:asists => },
+                     {:steals => },
+                     {:blocks => },
+                     {:slam_dunks => }]},
+
+    :away => {
+      :team_name => "",
+      :colors => [],
+      :players =>[{}] ,
+      }}
 
 
 
